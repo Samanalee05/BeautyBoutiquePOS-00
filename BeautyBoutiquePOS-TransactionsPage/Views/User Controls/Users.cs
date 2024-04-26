7fe29080fc882a29171b21a7003785cf1195791f
Samanalee05 <136576153+Samanalee05@users.noreply.github.com>
2024-04-26 14:10:18 +0530

    user delete
    add function to delete users form db

--- BeautyBoutiquePOS-TransactionsPage/Views/User Controls/Users.cs
+++ BeautyBoutiquePOS-TransactionsPage/Views/User Controls/Users.cs
@@ -24,6 +24,60 @@ namespace BeautyBoutiquePOS_TransactionsPage.Views.User_Controls
             styles.RoundCornerPanels(panel1, 10);
 
             LoadUsers();
+
+            DataGridViewButtonColumn deleteButtonColumn = new DataGridViewButtonColumn();
+            deleteButtonColumn.Name = "Delete";
+            deleteButtonColumn.HeaderText = "Delete";
+            deleteButtonColumn.Text = "Delete";
+            deleteButtonColumn.UseColumnTextForButtonValue = true;
+            dataGridView1.Columns.Add(deleteButtonColumn);
+            deleteButtonColumn.Width = 100;
+
+            dataGridView1.CellContentClick += dataGridViewCellContentClick;
+
+            
+        }
+
+        private void dataGridViewCellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == dataGridView1.Columns["Delete"].Index && e.RowIndex >= 0)
+            {
+                String nic = Convert.ToString(dataGridView1.Rows[e.RowIndex].Cells["nic"].Value);
+
+                Delete(nic);
+            }
+        }
+
+        private void Delete(String nic)
+        {
+            MySqlConnection connection = new MySqlConnection(DatabaseConnection.GetConnectionString());
+            string deleteQuery = "DELETE FROM users WHERE nic = @Nic";
+            using (MySqlCommand command = new MySqlCommand(deleteQuery, connection))
+            {
+                command.Parameters.AddWithValue("@Nic", nic);
+                try
+                {
+                    connection.Open();
+                    int rowsAffected = command.ExecuteNonQuery();
+                    if (rowsAffected > 0)
+                    {
+                        MessageBox.Show("user deleted successfully.");
+                        LoadUsers();
+                    }
+                    else
+                    {
+                        MessageBox.Show("No rows were deleted.");
+                    }
+                }
+                catch (MySqlException ex)
+                {
+                    MessageBox.Show("Error deleting user: " + ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
