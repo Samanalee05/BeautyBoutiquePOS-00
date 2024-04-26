dae3a4db3f85c20740fe145d818ad9e1eef08737
Samanalee05 <136576153+Samanalee05@users.noreply.github.com>
2024-04-26 14:02:44 +0530

    customers delete function
    add function to delete customers for db

--- BeautyBoutiquePOS-TransactionsPage/Views/User Controls/Customers.cs
+++ BeautyBoutiquePOS-TransactionsPage/Views/User Controls/Customers.cs
@@ -23,6 +23,58 @@ namespace BeautyBoutiquePOS_TransactionsPage.Views.User_Controls
             UserControlStyles styles = new UserControlStyles();
             styles.CustomizeDataGridView(customerGridView);
             styles.RoundCornerPanels(panel1, 10);
+
+            DataGridViewButtonColumn deleteButtonColumn = new DataGridViewButtonColumn();
+            deleteButtonColumn.Name = "Delete";
+            deleteButtonColumn.HeaderText = "Delete";
+            deleteButtonColumn.Text = "Delete";
+            deleteButtonColumn.UseColumnTextForButtonValue = true;
+            customerGridView.Columns.Add(deleteButtonColumn);
+            deleteButtonColumn.Width = 100;
+
+            customerGridView.CellContentClick += dataGridViewCellContentClick;
+        }
+
+        private void dataGridViewCellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == customerGridView.Columns["Delete"].Index && e.RowIndex >= 0)
+            {
+                String nic = Convert.ToString(customerGridView.Rows[e.RowIndex].Cells["nic"].Value);
+
+                Delete(nic);
+            }
+        }
+
+        private void Delete(String nic)
+        {
+            MySqlConnection connection = new MySqlConnection(DatabaseConnection.GetConnectionString());
+            string deleteQuery = "DELETE FROM customers WHERE nic = @Nic";
+            using (MySqlCommand command = new MySqlCommand(deleteQuery, connection))
+            {
+                command.Parameters.AddWithValue("@Nic", nic);
+                try
+                {
+                    connection.Open();
+                    int rowsAffected = command.ExecuteNonQuery();
+                    if (rowsAffected > 0)
+                    {
+                        MessageBox.Show("customer deleted successfully.");
+                        LoadCustomers();
+                    }
+                    else
+                    {
+                        MessageBox.Show("No rows were deleted.");
+                    }
+                }
+                catch (MySqlException ex)
+                {
+                    MessageBox.Show("Error deleting customer: " + ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
