c302ef9a14eef8e1be5d10c0190cbfdd40cc4755
Samanalee05 <136576153+Samanalee05@users.noreply.github.com>
2024-04-26 10:10:01 +0530

    change db
    add remote db connection string

--- BeautyBoutiquePOS-TransactionsPage/Class/DatabaseConnection.cs
+++ BeautyBoutiquePOS-TransactionsPage/Class/DatabaseConnection.cs
@@ -9,16 +9,15 @@ namespace BeautyBoutiquePOS_TransactionsPage.Class
 {
     internal class DatabaseConnection
     {
-        private const string Server = "localhost";
-        private const int Port = 3306;
-        private const string Database = "test";
-        private const string Username = "root";
-        private const string Password = "";
+        private const string Server = "mysql-15151bc9-samanala-e089.d.aivencloud.com";
+        private const int Port = 11450;
+        private const string Database = "defaultdb";
+        private const string Username = "avnadmin";
+        private const string Password = "AVNS_k-Wrpa4HswdWcem3LB_";
 
 
         public static string GetConnectionString()
         {
-
             return $"Server={Server};Port={Port};Database={Database};Uid={Username};Pwd={Password};";
         }
     }
